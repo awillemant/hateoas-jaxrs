e6302a12dac44ae0576b5deac1dd2078690a1337
awillemant <awillemant@users.noreply.github.com>
2014-05-29 14:51:48 +0200

    Modification des données dans la BDD

--- hateoas-webservice/src/main/java/awt/hateoas/entity/GenericEntity.java
+++ hateoas-webservice/src/main/java/awt/hateoas/entity/GenericEntity.java
@@ -1,13 +1,9 @@
 package awt.hateoas.entity;
 
-import java.util.Date;
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -25,14 +21,6 @@ public class GenericEntity {
 	@XmlAttribute
 	private long id;
 
-	@Column(name = "creation_time", nullable = false)
-	@XmlAttribute
-	private Date creationTime;
-
-	@Column(name = "modification_time", nullable = false)
-	@XmlAttribute
-	private Date modificationTime;
-
 	@XmlElement
 	@Transient
 	private RESTServiceDiscovery links;
@@ -46,30 +34,6 @@ public class GenericEntity {
 
 	public long getId() {
 		return id;
-	}
-
-
-	public Date getCreationTime() {
-		return creationTime;
-	}
-
-
-	public Date getModificationTime() {
-		return modificationTime;
-	}
-
-
-	@PreUpdate
-	public void preUpdate() {
-		modificationTime = new Date();
-	}
-
-
-	@PrePersist
-	public void prePersist() {
-		Date now = new Date();
-		creationTime = now;
-		modificationTime = now;
 	}
 
 
--- hateoas-webservice/src/main/resources/import.sql
+++ hateoas-webservice/src/main/resources/import.sql
@@ -1,9 +1,13 @@
-INSERT INTO `Conference` (`id`, `creation_time`, `modification_time`, `name`, `startDate`, `endDate`) VALUES (1, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Devoxx 2014', '2014-11-10 00:00:00', '2014-11-14 00:00:00');
-INSERT INTO `Conference` (`id`, `creation_time`, `modification_time`, `name`, `startDate`, `endDate`) VALUES (2, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Devoxx UK 2014', '2014-06-12 00:00:00', '2014-06-13 00:00:00');
-INSERT INTO `Conference` (`id`, `creation_time`, `modification_time`, `name`, `startDate`, `endDate`) VALUES (3, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Devoxx FR 2014', '2014-04-16 00:00:00', '2014-04-18 00:00:00');
+INSERT INTO `Conference` (`id`,  `name`, `startDate`, `endDate`) VALUES (1, "Take Off Conf 2013", '2013-01-17 00:00:00', '2013-01-18 00:00:00');
+INSERT INTO `Conference` (`id`,  `name`, `startDate`, `endDate`) VALUES (2, "Take Off Conf 2014", '2014-01-30 00:00:00', '2014-01-31 00:00:00');
+INSERT INTO `Conference` (`id`,  `name`, `startDate`, `endDate`) VALUES (3, "Devoxx 2014", '2014-11-10 00:00:00', '2014-11-14 00:00:00');
 
-INSERT INTO `Talk` (`id`, `creation_time`, `modification_time`, `speakerName`, `title`, `conference_id`) VALUES (1, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Paul Sandoz', 'Perchance to Stream with Java 8', 2);
-INSERT INTO `Talk` (`id`, `creation_time`, `modification_time`, `speakerName`, `title`, `conference_id`) VALUES (2, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'David Pilato', 'Advanced Search for Your Legacy Application', 2);
-INSERT INTO `Talk` (`id`, `creation_time`, `modification_time`, `speakerName`, `title`, `conference_id`) VALUES (3, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Josh Long', 'Have You Seen Spring Lately?',2);
-INSERT INTO `Talk` (`id`, `creation_time`, `modification_time`, `speakerName`, `title`, `conference_id`) VALUES (4, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'Grégory Paul', 'Bitcoin et monnaies cryptographiques',3);
-INSERT INTO `Talk` (`id`, `creation_time`, `modification_time`, `speakerName`, `title`, `conference_id`) VALUES (5, '2014-04-26 00:00:00', '2014-04-26 00:00:00', 'José Paumard', "50 nouvelles choses que l'on peut faire avec Java 8",3);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (1, "Jakob Mattsson", "You are not service oriented enough!", 1);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (2, "Olivier Lacan", "Science-based development", 1);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (3, "Xavier Coulon", "Build your website with awestruct and publish it on the cloud with Git", 1);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (4, "Rémi Parmentier", "Design for developers", 1);
+
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (5, "Luca Sale", "The bizarre app experiment", 2);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (6, "Paul Rouget", "Understanding a layout engine: parallelism and hardware acceleration", 2);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (7, "Ori Pekelman", "Zen and the art of Hypermedia design", 2);
+INSERT INTO `Talk` (`id`,  `speakerName`, `title`, `conference_id`) VALUES (8, "Tim Ruffles", "Building a tiny AngularJS", 2);
